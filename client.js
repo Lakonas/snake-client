00df198538b0d964d9257ac486364b80990e4cd1
Bill Katsoulis <bill.katsoulis@rogers.com>
2024-11-20 20:47:16 -0500

    Moved up

--- client.js
+++ client.js
@@ -9,9 +9,18 @@ const connect = function () {
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
+
+  conn.on('connect', (data) => {
+    console.log('Connected to server');
+
+    conn.write("Name: PDR"); //syntax send data from client to server
+    //conn.write("Move up");
+  });
+
   conn.on('data', (data) => {
     console.log('Server says: ', data);
   });
+
   return conn;
 };
 module.exports = connect;
